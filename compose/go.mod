3828726820037c487fbc2b5aac945da30c7d150f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2023-11-20 23:54:49 +0700

    Regenerate module to compat with Dagger 0.9.3
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- compose/go.mod
+++ compose/go.mod
@@ -5,8 +5,8 @@ go 1.21.3
 require (
 	github.com/99designs/gqlgen v0.17.31
 	github.com/Khan/genqlient v0.6.0
-	github.com/compose-spec/compose-go v1.19.0
-	golang.org/x/sync v0.3.0
+	github.com/compose-spec/compose-go v1.20.1
+	golang.org/x/sync v0.4.0
 )
 
 require (
@@ -20,13 +20,13 @@ require (
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
-	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/vektah/gqlparser/v2 v2.5.6 // indirect
-	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190905194746-02993c407bfb // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	golang.org/x/exp v0.0.0-20230713183714-613f0c0eb8a1 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	golang.org/x/exp v0.0.0-20231006140011-7918f672742d
+	golang.org/x/sys v0.13.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- compose/main.go
+++ compose/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"os"
+	"path/filepath"
 
 	"github.com/compose-spec/compose-go/loader"
 	"github.com/compose-spec/compose-go/types"
@@ -15,16 +16,19 @@ type Compose struct{}
 //
 // Returns a container with pre-setup services.
 func (m *Compose) WithFile(ctx context.Context, path string) (*Container, error) {
-	yaml, err := os.ReadFile(path)
+	p, err := filepath.Abs(path)
 	if err != nil {
 		return nil, err
 	}
-
+	yaml, err := os.ReadFile(p)
+	if err != nil {
+		return nil, err
+	}
 	wd, err := os.Getwd()
 	if err != nil {
 		return nil, err
 	}
-	// TODO: use filename from path.
+
 	proj, err := loader.LoadWithContext(
 		ctx,
 		types.ConfigDetails{
@@ -32,7 +36,7 @@ func (m *Compose) WithFile(ctx context.Context, path string) (*Container, error)
 			Version:    "3",
 			WorkingDir: wd,
 			ConfigFiles: []types.ConfigFile{
-				{Filename: "nofile", Content: yaml},
+				{Filename: filepath.Base(path), Content: yaml},
 			},
 			Environment: make(types.Mapping),
 		},
@@ -50,12 +54,13 @@ func (m *Compose) WithFile(ctx context.Context, path string) (*Container, error)
 	container := dag.Container()
 	for _, svc := range proj.Services {
 		svc := svc
-		container = container.WithServiceBinding(svc.Name, buildContainer(dag, svc))
+		container = container.WithServiceBinding(svc.Name, buildContainer(dag, svc).AsService())
 	}
 
 	return container, nil
 }
 
+// Construct container service from compose service spec.
 func buildContainer(client *Client, svc types.ServiceConfig) *Container {
 	container := client.Container().From(svc.Image)
 
