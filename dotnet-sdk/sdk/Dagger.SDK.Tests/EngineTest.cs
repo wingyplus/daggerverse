dd6a661d17e72bfad80df4d0a15b4a421bdc2793
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-05-01 23:34:46 +0700

    dotnet-sdk: add Engine class
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

---
+++ dotnet-sdk/sdk/Dagger.SDK.Tests/EngineTest.cs
@@ -0,0 +1,45 @@
+﻿using Dagger.SDK.GraphQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dagger.SDK.Tests;
+
+
+public class EngineTest
+{
+    [Fact]
+    public async void TestExecute()
+    {
+        var gqlClient = new GraphQLClient();
+        var queryBuilder = QueryBuilder
+            .Builder()
+            .Select("container")
+            .Select("from", [new Argument("address", new StringValue("alpine"))])
+            .Select("id");
+        var engine = new Engine();
+
+        string id = await engine.Execute<string>(gqlClient, queryBuilder);
+
+        Assert.NotEmpty(id);
+    }
+
+    [Fact]
+    public async void TestExecuteList()
+    {
+        var gqlClient = new GraphQLClient();
+        var queryBuilder = QueryBuilder
+            .Builder()
+            .Select("container")
+            .Select("from", [new Argument("address", new StringValue("alpine"))])
+            .Select("envVariables")
+            .Select("name");
+        var engine = new Engine();
+
+        var ids = await engine.ExecuteList<string>(gqlClient, queryBuilder);
+
+        Assert.NotEmpty(ids);
+    }
+}
---
+++ dotnet-sdk/sdk/Dagger.SDK/Engine.cs
@@ -0,0 +1,65 @@
+﻿using Dagger.SDK.GraphQL;
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Dagger.SDK;
+
+public class Engine
+{
+    /// <summary>
+    /// Execute a GraphQL request and deserialize data into `T`.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="client">A GraphQL client.</param>
+    /// <param name="queryBuilder">A QueryBuilder instance.</param>
+    /// <returns></returns>
+    public static async Task<T> Execute<T>(GraphQLClient client, QueryBuilder queryBuilder)
+    {
+        var jsonElement = await Request(client, queryBuilder);
+        jsonElement = TakeJsonElementUntilLast<T>(jsonElement, queryBuilder.Path);
+        return jsonElement.GetProperty(queryBuilder.Path.Last().Name).Deserialize<T>()!;
+    }
+
+    /// <summary>
+    /// Similar to Execute but return a list of data.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="client">A GraphQL client</param>
+    /// <param name="queryBuilder">A QueryBuilder instance.</param>
+    /// <returns></returns>
+    public static async Task<List<T>> ExecuteList<T>(GraphQLClient client, QueryBuilder queryBuilder)
+    {
+        var jsonElement = await Request(client, queryBuilder);
+        jsonElement = TakeJsonElementUntilLast<T>(jsonElement, queryBuilder.Path);
+        return jsonElement
+            .EnumerateArray()
+            .Select(elem => elem.GetProperty(queryBuilder.Path.Last().Name).Deserialize<T>()!)
+            .ToList();
+    }
+
+    private static async Task<JsonElement> Request(GraphQLClient client, QueryBuilder queryBuilder)
+    {
+        var query = queryBuilder.Build();
+        var response = await client.RequestAsync(query);
+        // TODO: handle error here.
+        var jsonElement = await response.Content.ReadFromJsonAsync<JsonElement>()!;
+        return jsonElement.GetProperty("data");
+    }
+
+    // Traverse jsonElement until the last element.
+    private static JsonElement TakeJsonElementUntilLast<T>(JsonElement jsonElement, ImmutableList<Field> path)
+    {
+        var json = jsonElement;
+        foreach (var fieldName in path.RemoveAt(path.Count - 1).Select(field => field.Name))
+        {
+            json = json.GetProperty(fieldName);
+        }
+        return json;
+    }
+}
