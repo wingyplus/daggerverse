0fdcf23c8a8504d109f60fe7bc001707c0cef811
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-05-20 15:27:12 +0700

    earthly: make earthly works
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- earthly/main.go
+++ earthly/main.go
@@ -6,39 +6,68 @@ import (
 	"context"
 )
 
+// TODO: support mTLS.
+// TODO: support custom registry auth.
+
+const workspacePath = "/workspace"
+
 func New(
 	// Source directory.
 	source *Directory,
 ) *Earthly {
-	return &Earthly{Source: source}
+	return &Earthly{Source: source, Version: "v0.8.11"}
 }
 
+// Earthly module for dagger.
 type Earthly struct {
-	Source *Directory
+	Source  *Directory
+	Version string
 }
 
+// Run earthly.
 func (m *Earthly) Run(ctx context.Context, args []string) error {
-	_, err := m.image().
-		WithServiceBinding("docker", m.docker()).
-		WithEnvVariable("DOCKER_HOST", "tcp://docker:2375").
-		WithMountedDirectory("/source", m.Source).
-		WithWorkdir("/source").
-		WithExec(append([]string{"earthly"}, args...), ContainerWithExecOpts{
-			ExperimentalPrivilegedNesting: true,
-		}).
+	_, err := m.WithEarthly().
+		WithExec(append([]string{"earthly"}, args...)).
 		Sync(ctx)
 
 	return err
 }
 
-func (m *Earthly) image() *Container {
+func (m *Earthly) WithEarthly() *Container {
+	config := `
+global:
+  tls_enabled: false
+`
 	return dag.Container().
-		From("docker:cli").
-		WithExec([]string{"apk", "add", "wget"}).
-		WithExec([]string{"wget", "https://github.com/earthly/earthly/releases/latest/download/earthly-linux-amd64", "-O", "/usr/local/bin/earthly"}).
-		WithExec([]string{"chmod", "+x", "/usr/local/bin/earthly"})
+		From("earthly/earthly:"+m.Version).
+		WithServiceBinding("dockerd", m.DockerEngine()).
+		WithServiceBinding("buildkitd", m.Buildkitd()).
+		WithEnvVariable("DOCKER_HOST", "tcp://dockerd:2375").
+		WithEnvVariable("NO_BUILDKIT", "1").
+		WithEnvVariable("EARTHLY_BUILDKIT_HOST", "tcp://buildkitd:8372").
+		WithEnvVariable("BUILDKIT_TLS_ENABLED", "false").
+		WithNewFile("/root/.earthly/config.yml", ContainerWithNewFileOpts{
+			Contents: config,
+		}).
+		WithMountedDirectory(workspacePath, m.Source).
+		WithWorkdir(workspacePath).
+		WithoutEntrypoint()
 }
 
-func (m *Earthly) docker() *Service {
-	return dag.Docker().Daemon().Service()
+func (m *Earthly) DockerEngine() *Service {
+	return dag.Docker().Engine(DockerEngineOpts{Version: "26"})
+}
+
+// Start the Earthly Buildkitd as a service.
+func (m *Earthly) Buildkitd() *Service {
+	return dag.Container().
+		From("earthly/buildkitd:"+m.Version).
+		WithEnvVariable("BUILDKIT_TCP_TRANSPORT_ENABLED", "true").
+		WithEnvVariable("BUILDKIT_TLS_ENABLED", "false").
+		WithExec([]string{"rootlesskit", "buildkitd"}, ContainerWithExecOpts{
+			InsecureRootCapabilities: true,
+		}).
+		WithoutEntrypoint().
+		WithExposedPort(8372).
+		AsService()
 }
